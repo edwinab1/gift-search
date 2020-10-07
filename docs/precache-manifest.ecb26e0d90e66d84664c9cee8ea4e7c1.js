4b16255f1f199dbbeadc07dfb16a1b998ebc8840
pepe <pepe@mail.es>
2020-10-07 08:14:51 -0400

    docs add

---
+++ docs/precache-manifest.ecb26e0d90e66d84664c9cee8ea4e7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23c5f830e2dd57ab0eef8605f0232d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe15d6ae6b8eae7384f9",
+    "url": "/static/css/main.24e86ee0.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe15d6ae6b8eae7384f9",
+    "url": "/static/js/main.d456f830.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
